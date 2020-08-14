269059e4a33bcfacebf5cac4a59855001ef69ae6
Code Pancito <codepancito@gmail.com>
2020-08-14 15:11:05 -0400

    ADDED SPINNER

--- app/src/main/java/com/example/desafio_09_01_ciclobike/CicloviaAdapter.kt
+++ app/src/main/java/com/example/desafio_09_01_ciclobike/CicloviaAdapter.kt
@@ -5,17 +5,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.desafio_09_01_ciclobike.databinding.ItemCicloviaBinding
 
 class CicloviaAdapter(var listCiclovia : MutableList<Ciclovia>) :
     RecyclerView.Adapter<CicloviaAdapter.CicloviaViewHolder>() {
 
+    /*
     class CicloviaViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val textViewNombreCiclovia : TextView = itemView.findViewById(R.id.textView_NombreCiclovia)
         val textViewComunaCiclovia : TextView = itemView.findViewById(R.id.textView_ComunaCiclovia)
     }
+    */
+
+    class CicloviaViewHolder(itemBinding : ItemCicloviaBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(ciclovia : Ciclovia) {
+            //TODO arreglar adapter con view binding
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CicloviaViewHolder {
-        return CicloviaViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_ciclovia, parent, false))
+        return CicloviaViewHolder(ItemCicloviaBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun getItemCount(): Int {
@@ -23,6 +32,8 @@ class CicloviaAdapter(var listCiclovia : MutableList<Ciclovia>) :
     }
 
     override fun onBindViewHolder(holderCiclovia: CicloviaViewHolder, position: Int) {
+        holderCiclovia.item
+
         holderCiclovia.textViewNombreCiclovia.text = listCiclovia.get(position).nombre
         holderCiclovia.textViewComunaCiclovia.text = listCiclovia.get(position).comuna
     }
--- app/src/main/java/com/example/desafio_09_01_ciclobike/MainActivity.kt
+++ app/src/main/java/com/example/desafio_09_01_ciclobike/MainActivity.kt
@@ -2,36 +2,40 @@ package com.example.desafio_09_01_ciclobike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.desafio_09_01_ciclobike.databinding.ActivityMainBinding
 import com.example.desafio_09_01_ciclobike.SetupCiclovias as Setup
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var buttonListLasCondes : Button
-    lateinit var buttonListAll : Button
-    lateinit var recyclerViewLista : RecyclerView
-    lateinit var cicloviaAdapter: CicloviaAdapter
-
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerViewLista = findViewById(R.id.recyclerView_ListaCiclovias)
-        recyclerViewLista.adapter = CicloviaAdapter(Setup().init() as MutableList<Ciclovia>)
-        recyclerViewLista.layoutManager = LinearLayoutManager(this)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        buttonListLasCondes = findViewById(R.id.button_List_LasCondes)
-        buttonListLasCondes.setOnClickListener {
-            recyclerViewLista.adapter = CicloviaAdapter(Setup().initLasCondes())
+        binding.recyclerViewListaCiclovias.adapter = CicloviaAdapter(Setup().init() as MutableList<Ciclovia>)
+        binding.recyclerViewListaCiclovias.layoutManager = LinearLayoutManager(this)
+
+        binding.buttonListLasCondes.setOnClickListener {
+            binding.recyclerViewListaCiclovias.adapter = CicloviaAdapter(Setup().initLasCondes())
         }
 
-        buttonListAll = findViewById(R.id.button_ListAll)
-        buttonListAll.setOnClickListener {
-            recyclerViewLista.adapter = CicloviaAdapter(Setup().init() as MutableList<Ciclovia>)
+        binding.buttonListAll.setOnClickListener {
+            binding.recyclerViewListaCiclovias.adapter = CicloviaAdapter(Setup().init() as MutableList<Ciclovia>)
         }
+
+        ArrayAdapter.createFromResource(this, R.array.spinner_Comunas_options, android.R.layout.simple_spinner_item)
+            .also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerComunas.adapter = adapter }
+
     }
 }
